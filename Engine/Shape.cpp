befebf8ad1c78df2e85cc74dacf21acf083a147a
zubeyir-bodur <zubeyir.bodur@ug.bilkent.edu.tr>
2022-10-19 02:17:51 +0300

    std::vector optimizations

--- Engine/Shape.cpp
+++ Engine/Shape.cpp
@@ -11,9 +11,9 @@ Shape* Shape::s_rectangle = new Shape();
 Shape::Shape(const std::vector<glm::vec3>& coords)
 {
 	ASSERT(coords.size() >= 3);
-	m_no_transform_vertex_positions = new std::vector<float>((coords.size() + 1) * NUM_COORDINATES);
+	m_no_transform_vertex_positions = new std::vector<float>;
+	m_no_transform_vertex_positions->reserve((coords.size() + 1) * NUM_COORDINATES);
 	glm::vec3 center(0.0f, 0.0f, 0.0f);
-
 	for (auto& coord : coords)
 	{
 		center.x += coord.x;
@@ -21,25 +21,26 @@ Shape::Shape(const std::vector<glm::vec3>& coords)
 		center.z += coord.z;
 	}
 	center /= (float)coords.size();
-
-	(*m_no_transform_vertex_positions)[0] = center.x;
-	(*m_no_transform_vertex_positions)[1] = center.y;
-	(*m_no_transform_vertex_positions)[2] = center.z;
-	for (unsigned int i = NUM_COORDINATES; i < m_no_transform_vertex_positions->size(); i++)
+	m_no_transform_vertex_positions->emplace_back(center.x);
+	m_no_transform_vertex_positions->emplace_back(center.y);
+	m_no_transform_vertex_positions->emplace_back(center.z);
+	for (unsigned int i = NUM_COORDINATES; i < m_no_transform_vertex_positions->capacity(); i++)
 	{
-		(*m_no_transform_vertex_positions)[i] = coords[i / 3 - 1][i % 3];
+		m_no_transform_vertex_positions->emplace_back(coords[i / 3 - 1][i % 3]);
 	}
 
 	m_vertex_array = new VertexArray;
 	m_vertex_buffer = new VertexBuffer(m_no_transform_vertex_positions->data()
 		, m_no_transform_vertex_positions->size() * sizeof(float));
 	m_vertex_array->add_buffer(*m_vertex_buffer, s_layout);
-	m_indices = new std::vector<unsigned int>(coords.size() + 2);
+	
+	m_indices = new std::vector<unsigned int>;
+	m_indices->reserve(coords.size() + 2);
 	for (unsigned int i = 0; i < coords.size() + 1; i++)
 	{
-		(*m_indices)[i] = i;
+		m_indices->emplace_back(i);
 	}
-	(*m_indices)[coords.size() + 1] = 1;
+	m_indices->emplace_back(1);
 	m_index_buffer = new IndexBuffer(m_indices->data(), m_indices->size());
 }
 
@@ -64,11 +65,9 @@ void Shape::push_back_vertex(const glm::vec3& model_pos)
 	delete m_vertex_buffer;
 	delete m_vertex_array;
 	delete m_index_buffer;
-	delete m_indices;
-
-	m_no_transform_vertex_positions->push_back(model_pos.x);
-	m_no_transform_vertex_positions->push_back(model_pos.y);
-	m_no_transform_vertex_positions->push_back(model_pos.z);
+	m_no_transform_vertex_positions->emplace_back(model_pos.x);
+	m_no_transform_vertex_positions->emplace_back(model_pos.y);
+	m_no_transform_vertex_positions->emplace_back(model_pos.z);
 	glm::vec3 center(0.0f, 0.0f, 0.0f);
 
 	for (unsigned int i = 0; i < num_vertices(); i+=3)
@@ -89,13 +88,8 @@ void Shape::push_back_vertex(const glm::vec3& model_pos)
 	m_vertex_buffer = new VertexBuffer(m_no_transform_vertex_positions->data()
 		, m_no_transform_vertex_positions->size() * sizeof(float));
 	m_vertex_array->add_buffer(*m_vertex_buffer, s_layout);
-
-	m_indices = new std::vector<unsigned int>(num_vertices() + 1);
-	for (unsigned int i = 0; i < num_vertices(); i++)
-	{
-		(*m_indices)[i] = i;
-	}
-	(*m_indices)[num_vertices()] = 1; // draws the last triangle
+	(*m_indices)[m_indices->size()-1] = num_vertices()-1;
+	m_indices->emplace_back(1);
 	m_index_buffer = new IndexBuffer(m_indices->data(), m_indices->size());
 }
 
@@ -123,22 +117,24 @@ void Shape::init_static_members(int width)
 
 	// Index buffer for a 2D quad
 	constexpr unsigned int num_indices = 6;
-	auto* quad_indices = new std::vector<unsigned int>{
-		0, 1, 2,
-		2, 3, 0
-	};
-	auto* tri_indices = new std::vector<unsigned int>{
-		0, 1, 2
-	};
+	auto* quad_indices = new std::vector<unsigned int>;
+	quad_indices->reserve(num_indices);
+	quad_indices->emplace_back(0); quad_indices->emplace_back(1); quad_indices->emplace_back(2);
+	quad_indices->emplace_back(2); quad_indices->emplace_back(3); quad_indices->emplace_back(0);
+
+	auto* tri_indices = new std::vector<unsigned int>;
+	tri_indices->reserve(num_indices/2);
+	tri_indices->emplace_back(0); tri_indices->emplace_back(1); tri_indices->emplace_back(2);
 
 	// Create the vertex buffer for a rectangle
 	constexpr unsigned int rect_num_vertices = 4;
-	auto* rectangle_positions = new std::vector<float>{
-		0.0f,				0.0f,				global_z_pos_2d,	// 0
-		init_shape_length,	0.0f,				global_z_pos_2d,	// 1
-		init_shape_length,	init_shape_length,	global_z_pos_2d,	// 2
-		0.0f,				init_shape_length,	global_z_pos_2d		// 3
-	};
+	auto* rectangle_positions = new std::vector<float>;
+	rectangle_positions->reserve(rect_num_vertices * NUM_COORDINATES);
+	rectangle_positions->emplace_back(0.0f);				rectangle_positions->emplace_back(0.0f);				rectangle_positions->emplace_back(global_z_pos_2d); // 0
+	rectangle_positions->emplace_back(init_shape_length);	rectangle_positions->emplace_back(0.0f);				rectangle_positions->emplace_back(global_z_pos_2d); // 1
+	rectangle_positions->emplace_back(init_shape_length);	rectangle_positions->emplace_back(init_shape_length);	rectangle_positions->emplace_back(global_z_pos_2d); // 2
+	rectangle_positions->emplace_back(0.0f);				rectangle_positions->emplace_back(init_shape_length);	rectangle_positions->emplace_back(global_z_pos_2d); // 3
+
 	auto* rect_va = new VertexArray;
 	auto* rect_vb = new VertexBuffer(rectangle_positions->data(),
 		rectangle_positions->size() * sizeof(float));
@@ -147,11 +143,12 @@ void Shape::init_static_members(int width)
 
 	// Vertex Buffer for an equilateral triangle
 	constexpr unsigned int tri_num_vertices = 3;
-	auto* equilateral_triangle_positions = new std::vector<float>{
-		init_shape_length / 2,	0.0f,								global_z_pos_2d,	// 0
-		0.0f,					sqrtf(3) * init_shape_length / 2,	global_z_pos_2d,	// 1
-		init_shape_length,		sqrtf(3) * init_shape_length / 2,	global_z_pos_2d,	// 2
-	};
+	auto* equilateral_triangle_positions = new std::vector<float>;
+	equilateral_triangle_positions->reserve(tri_num_vertices * NUM_COORDINATES);
+	equilateral_triangle_positions->emplace_back(init_shape_length / 2);	equilateral_triangle_positions->emplace_back(0.0f);								equilateral_triangle_positions->emplace_back(global_z_pos_2d); // 0
+	equilateral_triangle_positions->emplace_back(0.0f);						equilateral_triangle_positions->emplace_back(sqrtf(3) * init_shape_length / 2);	equilateral_triangle_positions->emplace_back(global_z_pos_2d); // 1
+	equilateral_triangle_positions->emplace_back(init_shape_length);		equilateral_triangle_positions->emplace_back(sqrtf(3) * init_shape_length / 2);	equilateral_triangle_positions->emplace_back(global_z_pos_2d); // 2
+
 	auto* eq_tri_va = new VertexArray;
 	auto* eq_tri_vb = new VertexBuffer(equilateral_triangle_positions->data(),
 		equilateral_triangle_positions->size() * sizeof(float));
